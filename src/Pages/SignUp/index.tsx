ee3785c53bc91223595f03fed6c72dd16173aa78
mishraanis <mishraanis@gmail.com>
2022-10-30 00:25:51 +0530

    file uploading work

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,29 +1,51 @@
+//@ts-nocheck
 import React from 'react'
 import NavBar from '../../Components/Generic_Navbar'
 import SignedIn_NavBar from '../../Components/SignedIn_NavBar'
 import axios, { Axios } from 'axios'
 export default function SignUp() {
-
+    
     //make an object to store the data
     const [data, setData] = React.useState({
-      email:
-      "anismishra200@gmail.com",
-      password1:
-      "inshallahboysplayedwell",
-      password2:
-      "inshallahboysplayedwell",
+      email: "anismishra200@gmail.com",
+      password1: "inshallahboysplayedwell",
+      password2: "inshallahboysplayedwell",
+      first_name: "sjcndscd",
+      last_name: "scdkdscn",
+      address: "",
+      date_of_birth: "21/07/2001",
+      category: "",
+      proof_of_identity: undefined,
+      proof_of_address: "",
     //   username: "",
     //   password1: "",
     //   password2: "",
     //   email: "",
     });
-
+    
+    function selectFile(event:any) {
+        // this.setState({
+        //   selectedFiles: event.target.files,
+        // });
+        console.log(event.target.files[0]);
+        let file = event.target.files[0];
+        console.log(file);
+        setData((prevState) => ({ ...prevState, proof_of_identity:file}));
+    }
+    
     function OnSubmit(){
         axios.post('http://localhost:8000/dj-rest-auth/registration/', {
             username: data.email,
             password1: data.password1,
             password2: data.password2,
             email: data.email,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            address: data.address,
+            date_of_birth: data.date_of_birth,
+            category: data.category,
+            proof_of_identity: data.proof_of_identity,
+            proof_of_address: data.proof_of_address,
           }, { headers: { 'Content-Type': 'application/json' } })
           .then(function (response) {
             console.log(response);
@@ -38,9 +60,9 @@ export default function SignUp() {
     <div>
             
         <div>
-            <SignedIn_NavBar/>
+            {/* <SignedIn_NavBar/> */}
             <NavBar/>
-            <div className='flex flex-col items-center justify-center h-[85vh] w-full'>
+            <div className='flex flex-col items-center justify-center w-full'>
                 <div className='font-nunitoExtraBold text-2xl'>
                     Sign Up
                 </div>
@@ -52,13 +74,41 @@ export default function SignUp() {
                     <input type='text' placeholder='Last Name' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
                     <input type='text' placeholder='Aadhar Number' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/> */}
                     <input type='email' placeholder='Email' value={data.email} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
-                    onChange={(e) => setData({...data, email: e.target.value})}
+                    onChange={(e) => setData((prevState) => ({ ...prevState, email: e.target.value}))}
                     />
                     <input type='password' placeholder='Password' value={data.password1} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
-                    onChange={(e) => setData({...data, password1: e.target.value})}
+                    onChange={(e) => setData((prevState) => ({ ...prevState, password1: e.target.value}))}
                     />
                     <input type='password' placeholder='Confirm Password' value={data.password2} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
-                    onChange={(e) => setData({...data, password2: e.target.value})}
+                    onChange={(e) => setData((prevState) => ({ ...prevState, password2: e.target.value}))}
+                    />
+                    <input type='text' placeholder='First Name' value={data.first_name} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full' 
+                    onChange={(e) => setData((prevState) => ({ ...prevState, first_name: e.target.value}))}
+                    />
+                    <input type='text' placeholder='Last Name' value={data.last_name} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData((prevState) => ({ ...prevState, last_name: e.target.value}))}
+                    />
+                    <input type='text' placeholder='Address' value={data.address} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData((prevState) => ({ ...prevState, address: e.target.value}))}
+                    />
+                    <input type='date' placeholder='Date of Birth' value={data.date_of_birth} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData((prevState) => ({ ...prevState, date_of_birth: e.target.value}))}
+                    />
+                    <input type='text' placeholder='Category' value={data.category} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData((prevState) => ({ ...prevState, category: e.target.value}))}
+                    />
+                    {/* //Upload a document as a proof of Identity  */}
+                    <input type="file" placeholder='Proof of Identity' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) =>{e.preventDefault(); selectFile(e)}} />
+                    {/* <input type='file' placeholder='Proof of Identity' value={data.proof_of_identity} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, proof_of_identity: e.target.value})}
+                    /> */}
+                    {/* //Upload a document as a proof of Address */}
+                    <input type='file' placeholder='Proof of Address' value={data.proof_of_address} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => {
+                        e.preventDefault();
+                        setData({...data, proof_of_address: e.target.files[0]})
+                    }}
                     />
                     <button className='bg-googleBlue bg-opacity-100 hover:bg-opacity-95 focus:opacity-75 text-white rounded-md p-2 my-2 w-1/2 font-nunitoSemiBold' type="submit">Submit</button>
                 </form>
