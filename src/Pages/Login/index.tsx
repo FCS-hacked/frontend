e311668f43d9523f9e08a665f967ad31efdab3ec
mishraanis <mishraanis@gmail.com>
2022-10-29 19:55:09 +0530

    signup and login done

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
 import NavBar from '../../Components/Generic_Navbar'
-
+import axios from 'axios';
 export default function Login() {
+    const [data, setData] = React.useState({
+        // username: "",
+        // email:"",
+        // password: "",
+        email:"jhvbfhdf@gmail.com",
+        password:"scdksjndcfkjdf",
+        username:"huibuhvbgh",
+    });
+    function OnSubmit(){
+        console.log(data)
+        axios.post('http://localhost:8000/dj-rest-auth/login/', {
+            username: data.username,
+            password: data.password,
+            email: data.email,
+          }, { headers: { 'Content-Type': 'application/json' } })
+          .then(function (response) {
+            console.log(response);
+            localStorage.setItem('access_token', response.data.access_token);
+            localStorage.setItem('refresh_token', response.data.refresh_token);
+            console.log(localStorage.getItem('access_token'));
+            console.log(localStorage.getItem('refresh_token'));
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
   return (
     //create a login page
     <div>
@@ -14,10 +41,22 @@ export default function Login() {
                 </div>
                 {/* //create a form */}
                 
-                <form className='flex flex-col items-center justify-center w-[300px]'>
-                    <input type='text' placeholder='Username' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
-                    <input type='password' placeholder='Password' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
-                    <button className='bg-googleBlue bg-opacity-20 text-white rounded-md p-2 my-2 w-1/2 font-nunitoSemiBold'>Submit</button>
+                <form className='flex flex-col items-center justify-center w-[300px]'
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    OnSubmit();
+                }}
+                >
+                    <input type='text' value={data.username} placeholder='Username' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, username: e.target.value})}
+                    />
+                    <input type='email' value={data.email} placeholder='Email' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, email: e.target.value})}
+                    />
+                    <input type='password' value={data.password} placeholder='Password' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, password: e.target.value})}
+                    />
+                    <button type='submit' className='bg-googleBlue bg-opacity-100 hover:bg-opacity-95 focus:bg-opacity-80 text-white rounded-md p-2 my-2 w-1/2 font-nunitoSemiBold'>Submit</button>
                 </form>
                 <div className='flex flex-col items-center justify-center w-[300px]'>
                     <div className='font-nunitoSemiBold text-sm'>
--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -3,16 +3,38 @@ import NavBar from '../../Components/Generic_Navbar'
 import SignedIn_NavBar from '../../Components/SignedIn_NavBar'
 import axios, { Axios } from 'axios'
 export default function SignUp() {
+
+    //make an object to store the data
+    const [data, setData] = React.useState({
+      email:
+      "anismishra2001@gmail.com",
+      password1:
+      "inshallahboysplayedwell",
+      password2:
+      "inshallahboysplayedwell",
+      username:
+      "waahbe"
+    //   username: "",
+    //   password1: "",
+    //   password2: "",
+    //   email: "",
+    });
+
     function OnSubmit(){
-        axios.post('http://localhost:8000/api/users/signup', {
-            // email: 
-          })
+        axios.post('http://localhost:8000/dj-rest-auth/registration/', {
+            username: data.username,
+            password1: data.password1,
+            password2: data.password2,
+            email: data.email,
+          }, { headers: { 'Content-Type': 'application/json' } })
           .then(function (response) {
             console.log(response);
           })
           .catch(function (error) {
             console.log(error);
           });
+
+        console.log(data)
     }
   return (
     <div>
@@ -24,19 +46,27 @@ export default function SignUp() {
                 <div className='font-nunitoExtraBold text-2xl'>
                     Sign Up
                 </div>
-                <form className='flex flex-col items-center justify-center w-[300px]'>
+                <form className='flex flex-col items-center justify-center w-[300px]' onSubmit={(e) => {
+                  e.preventDefault();
+                  OnSubmit();}
+                }>
                     {/* <input type='text' placeholder='First Name' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
                     <input type='text' placeholder='Last Name' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
                     <input type='text' placeholder='Aadhar Number' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/> */}
-                    <input type='text' placeholder='Username' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
-                    <input type='text' placeholder='Email' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
-                    <input type='password' placeholder='Password' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
-                    <input type='password' placeholder='Confirm Password' className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'/>
-                    <button className='bg-googleBlue bg-opacity-100 hover:bg-opacity-95 focus:opacity-75 text-white rounded-md p-2 my-2 w-1/2 font-nunitoSemiBold'>Submit</button>
+                    <input type='text' placeholder='Username' value={data.username} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, username: e.target.value})}
+                    />
+                    <input type='email' placeholder='Email' value={data.email} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, email: e.target.value})}
+                    />
+                    <input type='password' placeholder='Password' value={data.password1} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, password1: e.target.value})}
+                    />
+                    <input type='password' placeholder='Confirm Password' value={data.password2} className='border-2 border-gray-300 rounded-md p-2 my-2 w-full'
+                    onChange={(e) => setData({...data, password2: e.target.value})}
+                    />
+                    <button className='bg-googleBlue bg-opacity-100 hover:bg-opacity-95 focus:opacity-75 text-white rounded-md p-2 my-2 w-1/2 font-nunitoSemiBold' type="submit">Submit</button>
                 </form>
-                
-                
-                
             </div>
         </div>
     </div>
