5c8dcd663c5db1c6380fb35ee5e18e0878fd6205
Heemank Verma <heemankv@gmail.com>
2022-11-25 16:56:17 +0530

    updates that anis cannot handle

--- src/Pages/Cart/index.tsx
+++ src/Pages/Cart/index.tsx
@@ -4,16 +4,17 @@ import Orders from '../Orders';
 export default function Cart() {
 
   const [orderDetails, setOrderDetails] = React.useState<any>({});
-
+  const [order_id, setOrderId] = React.useState<any>();
   useEffect(() => {
     const url = window.location.href;
     const orderId = url.split('?')[1].split('=');
     console.log(orderId, " orderId");
     if(orderId[1] !== undefined){
-    axios.get("http://localhost:8000/products/self/orders/" + orderId[1], {headers:{"Authorization": localStorage.getItem("token")}}).then((res) => {
-      console.log(res.data);
-      setOrderDetails(res.data);
-    });
+      setOrderId(orderId[1]);
+      axios.get("http://localhost:8000/products/self/orders/" + orderId[1], {headers:{"Authorization": localStorage.getItem("token")}}).then((res) => {
+        console.log(res.data);
+        setOrderDetails(res.data);
+      });
     }
     else {
       alert("No Order Found");
@@ -25,7 +26,7 @@ export default function Cart() {
       <div>
         <h1>Cart</h1>
         <h3>Total: {orderDetails.price}</h3>
-       <Orders amount={orderDetails.price} orderId={'temp'} prefills={"none"} />
+       <Orders amount={orderDetails.price} orderId={order_id} prefills={"none"} />
       </div>
     )
   }
@@ -33,7 +34,7 @@ export default function Cart() {
     return (
       <div>
         <h1>Cart</h1>
-        <p>Cart is empty</p>
+        <p>Cart is empty or invalid order id</p>
       </div>
     )
   }
