e58173873d3d95a6c1086b0f79680c45bf48aabd
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-12-01 23:25:52 +0530

    minor

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,14 +3,14 @@ import NavBar from '../../Components/Generic_Navbar'
 export default function Home() {
   return (
     <div>
-        <div>
+        <div className="h-screen">
             <NavBar/>
             <div className="flex justify-center text-center font-nunitoBold text-4xl align-middle mt-[30vh]">
               Welcome to the home page<br/>
               This is group 2's project
 
             </div>
-            <div className="text-sm text-gray">Please accept the certificate at <a href="https://192.168.2.234:8000">https://192.168.2.234:8000</a> if you face errors</div>        
+            <div className="text-xs text-gray position-fixed bottom-0 right-0">Please accept the certificate at <a href="https://192.168.2.234:8000">https://192.168.2.234:8000</a> if you face errors</div>
       </div>
     </div>
   )
