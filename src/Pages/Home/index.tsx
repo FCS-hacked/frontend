c2c94efd669282ca201918b2cd2726dd8ea3f0c3
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-12-01 23:26:29 +0530

    minor

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
               This is group 2's project
 
             </div>
-            <div className="text-xs text-gray position-fixed bottom-0 right-0">Please accept the certificate at <a href="https://192.168.2.234:8000">https://192.168.2.234:8000</a> if you face errors</div>
+            <div className="text-sm text-gray position-fixed bottom-0 right-0">Please accept the certificate at <a href="https://192.168.2.234:8000">https://192.168.2.234:8000</a> if you face errors</div>
       </div>
     </div>
   )
